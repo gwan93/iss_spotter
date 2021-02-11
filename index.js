2014f31d7f006f18150f649e9c89e661023195f6
vagrant <gio_wan@live.ca>
2021-02-11 21:38:13 +0000

    added functionality to grab lat/long based on an ip address

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
 });
 
-module.exports = { fetchMyIP };
+
+fetchCoordsByIP('68.147.5.95', (error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('Your coordinates are [lat, long] :', data);
+});
+
--- iss.js
+++ iss.js
@@ -1,14 +1,13 @@
 const request = require('request');
 
 const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
   const url = 'https://api.ipify.org/?format=json';
   request(url, (error, response, body) => {
 
     if (error) {
       callback(error, null);
       return;
-    } 
+    }
 
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
@@ -16,11 +15,34 @@ const fetchMyIP = function(callback) {
       return;
     }
 
-    
     const ipv4 = JSON.parse(body)['ip'];
     callback(null, ipv4);
-    
+
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+
+    } else if (response.statusCode !== 200) {
+      callback(Error('Bad status code.'), null);
+
+    } else {
+      const parsed = JSON.parse(body);
+      const latitude = parsed['latitude'];
+      const longitude = parsed['longitude'];
+      const coordinates = [latitude, longitude];
+      // console.log(coordinates);
+      callback(null, coordinates);
+
+    }
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
